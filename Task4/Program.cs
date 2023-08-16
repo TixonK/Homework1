262ec5c997bc6036e74d49122c383f02577afe1e
«Tihon <tihonkocerov@gmail.com>
2023-08-16 20:45:57 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+if ((a>b) & (a>c))
+{
+    Console.WriteLine("Maxx = ");
+    Console.WriteLine(a);
+}
+
+else if ((b>a) & (b>c))
+{
+    Console.WriteLine("Maxx = ");
+    Console.WriteLine(b);
+}
+
+else 
+{
+    Console.WriteLine("Maxx = ");
+    Console.WriteLine(c);
+}
